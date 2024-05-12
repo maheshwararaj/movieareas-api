e7dc7668c7fd34b4c893a7ee13bebf2caee675ed
maheshwararaj07 <maheshwararaj2003@gmail.com>
2024-05-12 10:00:17 +0530

    Added controller files

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,26 +1,18 @@
 import express from 'express';
-
+import { MovieCreate, MovieIndex, MovieUpdate,MovieDelete } from '../controllers/movies.controller';
 const router = express.Router()
 
 // crud functionality of movies
-router.get('/',(req,res)=>{
-  res.send("get all movie list")
-})
+router.get('/',MovieIndex )
 
 //c for creating movies
 
-router.post('/',(req,res)=>{
-    res.send("create a movie")
-})
+router.post('/',MovieCreate)
 
 //u - for updating movies
-router.put('/:id',(req,res)=>{
-    res.send("update a movie")
-})
+router.put('/:id',MovieUpdate)
 
 //d deleting movies
-router.delete('/:id',(req,res)=>{
-    res.send("delete a movie")
-})
+router.delete('/:id',MovieDelete)
 
 export default router;
